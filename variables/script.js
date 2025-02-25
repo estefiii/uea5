4d520ebbff17543d9dbd7d2975d35e2df8ddfbea
Estefi <2233072978@correo.ler.uam.mx>
2025-02-25 08:36:32 -0600

    Mi primer commit desde VS Code

---
+++ variables/script.js
@@ -0,0 +1,9 @@
+// variables
+
+let ingrediente1 = "azúcar";
+let precio = 46;
+
+// document.write(ingrediente1;)
+document.getElementById('demo').innerHTML = "<b>Un ingrediente es: </b>" + ingrediente1;
+
+document.getElementById('pre').innerHTML = "El precio del " + ingrediente1 + " es " + precio;

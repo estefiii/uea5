a3075ebca0a8ea32a5511b94be7f0fb6edde6b9a
steph <2233072978@correo.ler.uam.mx>
2025-03-07 21:31:11 -0600

    Agregue tarea 2

--- tarea2/script.js
+++ tarea2/script.js
@@ -1,10 +1,9 @@
-let p1="Párrafo 1 escribir aqui"
+let p1="Estas tecnologías son algo que antes de empezar la carrera solía pensar que no era necesario en mi vida, pero conforme voy avanzando me he dado cuenta que las tecnologías de HTML, CSS y JavaScript son algo realmente fundamental en mi formación, no solo para aprender cómo hacerlo sino para que en un futuro pueda realizar proyectos y formar un buen portafolio con estos, son bases para muchas cosas y el conocer estas tecnologías y haberlas trabajado me ayuda a asegurar un empleo en mi futuro. Aparte de que son una gran herramienta para poder transmitir distintos tipos de proyectos, desde una perspectiva artística hasta algo un poco más informativo."
 
-let p2="Párrafo 2 escribir aca"
-
-
-
+let p2="Estas tecnologías con su accesibilidad y flexibilidad hacen que cualquier persona con conexión a Internet pueda desarrollar proyectos y que lleguen más allá de lo académico, estos proyectos pueden llegar a ser compartidos hasta de una manera global. Facilitan la posibilidad de experimentar y jugar con una estructura ya determinada y llevarlo a la interacción y el arte en la web, especialmente como estamos viendo este trimestre, con proyectos narrativos dando nuevas formas e ideas sobre cómo es que podemos contar nuestras historias."
 
 document.getElementById('parr1').innerHTML=p1;
-
 document.getElementById('parr2').innerHTML=p2;
+
+console.log(p1.length);
+console.log(p2.length);

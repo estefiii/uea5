12d3d16b0904d7f664537e9e1e8fa38410e4e02c
Estefi <2233072978@correo.ler.uam.mx>
2025-03-18 08:39:05 -0600

    Cadenas

---
+++ cadenas/script.js
@@ -0,0 +1,17 @@
+// variable con texto
+let variable = "Texto de ejercicio";
+
+let variable2 = variable.toUpperCase();
+let variable3 = variable2.replace("TEXTO", "FRASES");
+// document
+document.getElementById('cadena').innerHTML=variable;
+document.getElementById('cadena2').innerHTML=variable2;
+document.getElementById('cadena3').innerHTML=variable3;
+
+//console
+console.log("Hola");
+console.log(variable.length);
+console.log(variable.at(10));
+
+
+window.alert("oli jiji");
